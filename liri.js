ed29136240d2833877c3398702937b2d06fe60d1
Raghav Murthy <raghavgm@gmail.com>
2016-04-08 22:00:39 -0400

    initial commit

---
+++ liri.js
@@ -0,0 +1,9 @@
+var keys = require('./keys.js');
+
+// To get movie info, I would use the npm request package
+// var request = require('request');
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage. 
+//   }
+// })
